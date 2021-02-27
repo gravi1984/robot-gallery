bf66f6bccbac6aecbe631304c90d1a6135d993b5
gravi2015@outlook.com <gravi2015@outlook.com>
2021-02-27 20:33:11 +1100

    useContext(exportedContext)

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Robot.module.css';
 import { appContext } from "../index";
+
 
 interface RobotProps {
     id: number,
@@ -10,21 +11,22 @@ interface RobotProps {
 
 // ES5 object => { f1, f2, f3 }
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
-  
+
+    // use context export from index
+    const value = useContext(appContext);
+
     return (
-        <appContext.Consumer>
-            {(value) => {
-                return (<div className={styles.robotItem}>
-                    <img alt="robot" src={`https://robohash.org/${id}`} />
-                    <h2>{name}</h2>
-                    <p>{email}</p>
-                    <p>author: {value.username}</p>
-                </div>);
-            }
 
-            }
+        <div className={styles.robotItem}>
+            <img alt="robot" src={`https://robohash.org/${id}`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+            <p>author: {value.username}</p>
+        </div>
 
-        </appContext.Consumer>
+
+
+
 
     );
 };
